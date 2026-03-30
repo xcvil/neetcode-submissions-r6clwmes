53414b33e829971015dd4b8a862d04b6306c6972
Xiaochen Zheng <68646214+xcvil@users.noreply.github.com>
2026-03-30 11:00:12 +0200

    Add: evaluate-division - submission-4

---
+++ Data Structures & Algorithms/evaluate-division/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = collections.defaultdict(set)
+
+        for equ, value in zip(equations, values):
+            var1, var2 = equ
+            graph[var1].add((var2, value))
+            graph[var2].add((var1, 1/value))
+
+        def dfs(start, end, accumulates):
+            if start not in graph or end not in graph:
+                return None
+                
+            if start == end:
+                return accumulates
+            visited.add(start)
+            for nei, cnv in graph[start]:
+                if nei not in visited:
+                    res = dfs(nei, end, accumulates*cnv)
+                    if res is not None:
+                        return res
+
+            return None
+
+        ans = []
+        for start, end in queries:
+            visited = set()
+            res = dfs(start, end, 1)
+            if not res:
+                ans.append(-1)
+            else:
+                ans.append(res)
+
+        return ans
