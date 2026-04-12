a2154e75bdd1d0f719795183a54acc4d3d039903
Xiaochen Zheng <68646214+xcvil@users.noreply.github.com>
2026-04-13 00:45:02 +0200

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        import heapq
+
+        graph = collections.defaultdict(set)
+
+        for u, v, t in times:
+            graph[u].add((v, t))
+        dist = {node: float("inf") for node in range(1, n + 1)}
+
+        heap = [(0, k)]
+        dist[k] = 0
+        while heap:
+            cost, u = heapq.heappop(heap)
+
+            for v, w in graph[u]:
+                new_dist = w + cost
+                if new_dist < dist[v]:
+                    dist[v] = new_dist
+                    heapq.heappush(heap, (new_dist, v)) 
+        res = max(dist.values())
+        return res if res < float('inf') else -1  
+
